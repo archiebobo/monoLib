092eec835ae6f6ffd1ba1970c0e2434eef80e13d
Archie <archie@monodb.com>
2021-01-14 21:59:02 +0800

    20200114 add branch Archie

--- MonoLib/MonoLib/mono_Vector.h
+++ MonoLib/MonoLib/mono_Vector.h
@@ -3,13 +3,15 @@
 #ifndef _MONO_VECTOR_H_
 #define _MONO_VECTOR_H_
 
+
+
+
 struct MoVector2i
 {
 	int val[2];
 	int* x;
 	int* y;
 };
-
 struct MoVecter3i 
 {
 	int val[3];
@@ -17,8 +19,7 @@ struct MoVecter3i
 	int* y;
 	int* z;
 };
-
 struct MoVector2i createMono2i(int x, int y);
-
 struct MoVector3i createMono3i(int x, int y);
+
 #endif // !_MONO_VECTOR_H_
